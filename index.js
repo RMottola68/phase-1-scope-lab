2a9731e919eebd3cbce77b82bdf2c3c08c5655fc
Ryan Mottola <rmottola68@gmail.com>
2022-06-07 16:40:21 -0400

    commit tree 45a1adfe7099da23417426adf9ac1e46e5371d46

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = '';
+// var bestCustomer;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'literally everyone';
+    return leastFavoriteCustomer;
+}
